ee716c15df3866fe6a6e72b5759e9dd508770c79
Eric Turner <equality509@gmail.com>
2022-12-23 10:48:49 -0500

    day 1

--- src/App.js
+++ src/App.js
@@ -1,25 +1,71 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import Uncontrolled from "./components/Uncontrolled";
 
-function App() {
+export default function App() {
+  const [skills, setSkills] = useState([{ skill: "JavaScript", level: 4 }])
+
+  const [form, setForm] = useState({
+    skill: "",
+    level: "3"
+  })
+
+  function addSkill(event) {
+    console.log(event)
+    // prevent the refreshing
+    event.preventDefault()
+    //console.log the form state
+    console.log(form)
+    // create a copy of skills
+    const newState = [...skills]
+    // add the new skill
+    newState.push(form)
+    // update the state
+    setSkills(newState)
+    // reset the form
+    setForm({
+      skill: "",
+      level: "3"
+    })
+  }
+
+  function handleChange(event){
+  //  const newState = {...form} // making a copy of current state
+  //  newState[event.target.name] = event.target.value // update the copy
+  //  setForm(newState) // make the copy the new state
+  setForm({
+    ...form, 
+    [event.target.name]: event.target.value
+  })
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <section>
+      <Uncontrolled/>
+      <h2>DEV SKILLS</h2>
+      <hr />
+      {skills.map((s) => (
+        <article key={s.skill}>
+          <div>{s.skill}</div> <div>{s.level}</div>
+        </article>
+      ))}
+      <hr />
+      <form onSubmit={addSkill}>
+        <label>
+          <span>SKILL</span>
+          <input name="skill" value={form.skill} onChange={handleChange}/>
+        </label>
+        <label>
+          <span>LEVEL</span>
+          <select name="level" value={form.level} onChange={handleChange}>
+            <option value="1">1</option>
+            <option value="2">2</option>
+            <option value="3">3</option>
+            <option value="4">4</option>
+            <option value="5">5</option>
+          </select>
+        </label>
+        <button>ADD SKILL</button>
+      </form>
+    </section>
   );
 }
-
-export default App;
